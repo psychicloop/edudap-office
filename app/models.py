ebdfd3e79533c4604dbe10739e56325e4c4d7770
psychicloop <sharma.pratik@edudap.com>
2026-01-20 16:21:24 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -83,3 +83,12 @@ class Todo(db.Model):
     due_date = db.Column(db.DateTime, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     user = db.relationship('User', backref='todos')
+
+# --- THIS WAS THE MISSING PIECE ---
+class LocationPing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+    user = db.relationship('User', backref='pings')
